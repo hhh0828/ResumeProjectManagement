5e4370008732ed3cc3bbe43ea0605acc53d14b86
hyunho.hong <hhhcjswo@naver.com>
2024-08-26 18:24:45 +0900

    "update" to do for nextday

--- Main.go
+++ Main.go
@@ -8,3 +8,44 @@ func main() {
 	http.ListenAndServe("0.0.0.0:8700", NewHandlers())
 
 }
+
+/*
+Master Node
+laptop - Asus G14
+requirement below
+
+kube
+ccm
+cm
+etcd
+apiserver
+
+
+nodes
+requirement below
+
+kubelet
+kubeadm
+docker CRI
+
+
+worker node1번
+laptop- samsung
+Pod1
+Volume1 resource provider - Local directory mount 사용
+
+Pod2
+Container4 - ImgFS - transfer file. when the
+
+
+worker node2번
+desktop- 10940x
+
+Pod1
+Container1 - NginX Web Server - proxy and transfer static file. port 80 to 8800 / Pod2 IP address
+Container2 - Resumemanagement Server - Web application server port 8800
+Pod2 -
+Container3 - PostgreDB - DB server
+https://kubernetes.io/ko/docs/tutorials/kubernetes-basics/create-cluster/cluster-intro/
+
+*/
