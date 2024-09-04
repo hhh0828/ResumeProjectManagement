2da41407df4b9a54d6832a150ecac513f1c11f56
hyunho.hong <hhhcjswo@naver.com>
2024-09-05 00:24:24 +0900

    TOdolist on Main

--- Main.go
+++ Main.go
@@ -12,11 +12,11 @@ func main() {
 /* 0905 todo Dynamic Infra 환경 구축
 
 1. AWS > EC2. T2.Micro > nginX LT Container 설치 - completed
-2. nginX인증서 설치 - not completed
-3. AWS 공인아이피 도메인 등록 - completed, wating for 24hours before it acrivate itself
-4. nginX proxy pass 설정 - not completed - after domain registered on the DNS servers globally
-5. 쿠버네티스 로컬환경 인그레스 서비스 설정 - not completed need to case study.
-6. 로컬환경 방화벽 설정 포트포워딩 - not completed.
+2. nginX 및 인증서 설치 - completed
+3. AWS 공인아이피 도메인 등록 - completed
+4. nginX proxy pass 설정 - completed - after domain registered on the DNS servers globally
+[5. 쿠버네티스 로컬환경 인그레스 서비스 설정 - not completed need to case study.]
+6. 로컬환경 방화벽 설정 포트포워딩 - completed
 
 7. resumemanagement app Helm Package and deploying -
 [options]
@@ -25,6 +25,21 @@ func main() {
 7-2. add a pod with above func and change service.yaml.
 // 배포할 deployment의 service 매니페스트의 로컬 부분을 수정한다.
 // 수정후 배포 완료하고 NginX에 OK사인 전달, 서비스 재개
+
+
+7-3. 업로드 및 DB 관련 추가 api 개발
+7-4. 리팩토링
+7-5. 개선점 피드백 받기
+7-6. 개선점 픽스
+7-7. 아키텍팅 도식화 추상화된 내용 정리 및 resumesite에 업로드
+
+//8. Kubernetes network 정복
+//9. Kubernetes statefulset 정복
+//10. Kubernetes & Jenkins,helm,Workload 정복
+//11. Kuber & prometheus 정복
+//12. AWS cloud
+
+
 */
 /*
 Master Node
