9e468e3a6322ba792112d11cd048c72e54cec156
hyunho.hong <hhhcjswo@naver.com>
2024-09-08 21:29:32 +0900

    d

--- home/sklang/sklangscripts.js
+++ home/sklang/sklangscripts.js
@@ -5,64 +5,61 @@ document.addEventListener("DOMContentLoaded", function () {
     const loadMoreButton = document.getElementById('loadMoreButton');
     const experiencesPerPage = 2; // 한 번에 표시할 경험의 수
 
-    // API로부터 JSON 데이터 가져오기
+    function displayExperience() {
+        const endIndex = Math.min(currentIndex + experiencesPerPage, allExperienceData.length);
+
+        for (let i = currentIndex; i < endIndex; i++) {
+            const experience = allExperienceData[i];
+
+            const experienceCard = ` 
+                <div class="card shadow border-0 rounded-4 mb-5" data-id="${experience.ID}">
+                    <div class="card-body p-5">
+                        <div class="row align-items-center gx-5">
+                            <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                <div class="bg-light p-4 rounded-4">
+                                    <div class="date-container">
+                                        <div class="text-primary fw-bolder">${experience.period}</div>
+                                    </div>
+                                    <div class="text-container">
+                                        <div class="small fw-bolder">${experience.role}</div>
+                                        <div class="small text-muted">${experience.company}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+                                <div>${experience.description}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-light position-absolute top-0 start-0 m-2 p-1 edit-button" 
+                    style="border-radius: 50%; z-index: 10;">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                </div>
+            `;
+
+            experienceContainer.insertAdjacentHTML('beforeend', experienceCard);
+        }
+
+        currentIndex = endIndex;
+
+        if (currentIndex >= allExperienceData.length) {
+            loadMoreButton.style.display = 'none';
+        }
+    }
+
     fetch('/returnresume')
         .then(response => response.json())
         .then(data => {
             allExperienceData = data.exps;
-            displayExperience(); // 페이지 로드 시 초기 두 개의 경험만 표시
-
-            function displayExperience() {
-                const endIndex = Math.min(currentIndex + experiencesPerPage, allExperienceData.length);
-
-                for (let i = currentIndex; i < endIndex; i++) {
-                    const experience = allExperienceData[i];
-
-                    const experienceCard = ` 
-                        <div class="card shadow border-0 rounded-4 mb-5" data-id="${experience.ID}">
-                            <div class="card-body p-5">
-                                <div class="row align-items-center gx-5">
-                                    <div class="col text-center text-lg-start mb-4 mb-lg-0">
-                                        <div class="bg-light p-4 rounded-4">
-                                            <div class="date-container">
-                                            <div class="text-primary fw-bolder">${experience.period}</div>
-                                            </div>
-                                            <div class="text-container">
-                                            <div class="small fw-bolder">${experience.role}</div>
-                                            <div class="small text-muted">${experience.company}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-8">
-                                        <div>${experience.description}</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <button class="btn btn-light position-absolute top-0 start-0 m-2 p-1 edit-button" 
-                            style="border-radius: 50%; z-index: 10;">
-                            <i class="bi bi-pencil"></i>
-                            </button>
-                        </div>
-                    `;
-
-                    experienceContainer.insertAdjacentHTML('beforeend', experienceCard);
-                }
-
-                currentIndex = endIndex;
-
-                // 모든 경험을 다 표시했으면 "더보기" 버튼 숨기기
-                if (currentIndex >= allExperienceData.length) {
-                    loadMoreButton.style.display = 'none';
-                }
-            }
-
-            loadMoreButton.addEventListener('click', function () {
-                displayExperience(); // 버튼 클릭 시 추가 데이터 로드
-            });
+            displayExperience();
         })
         .catch(error => console.error('Error fetching data:', error));
 
-    // 업로드 버튼 클릭 시 새로운 카드 열기
+    loadMoreButton.addEventListener('click', function () {
+        displayExperience();
+    });
+
     document.getElementById('upload').addEventListener('click', function () {
         const uniqueUploadId = `uploadCard-${Date.now()}`;
         const newCard = `
@@ -71,23 +68,23 @@ document.addEventListener("DOMContentLoaded", function () {
                     <h3 class="fw-bolder mb-4">Add New Experience</h3>
                     <form id="uploadForm-${uniqueUploadId}">
                         <div class="mb-3">
-                            <label for="periodStart" class="form-label">Start Date</label>
+                            <label for="periodStart-${uniqueUploadId}" class="form-label">Start Date</label>
                             <input type="month" class="form-control" id="periodStart-${uniqueUploadId}" required>
                         </div>
                         <div class="mb-3">
-                            <label for="periodEnd" class="form-label">End Date</label>
+                            <label for="periodEnd-${uniqueUploadId}" class="form-label">End Date</label>
                             <input type="month" class="form-control" id="periodEnd-${uniqueUploadId}" required>
                         </div>
                         <div class="mb-3">
-                            <label for="role" class="form-label">Role</label>
+                            <label for="role-${uniqueUploadId}" class="form-label">Role</label>
                             <input type="text" class="form-control" id="role-${uniqueUploadId}" required>
                         </div>
                         <div class="mb-3">
-                            <label for="company" class="form-label">Company</label>
+                            <label for="company-${uniqueUploadId}" class="form-label">Company</label>
                             <input type="text" class="form-control" id="company-${uniqueUploadId}" required>
                         </div>
                         <div class="mb-3">
-                            <label for="description" class="form-label">Description</label>
+                            <label for="description-${uniqueUploadId}" class="form-label">Description</label>
                             <textarea class="form-control" id="description-${uniqueUploadId}" rows="3" required></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
@@ -96,27 +93,26 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             </div>
         `;
-    
-        experienceContainer.insertAdjacentHTML('afterbegin', newCard); // 카드를 가장 위로 추가
-    
+
+        experienceContainer.insertAdjacentHTML('afterbegin', newCard);
+
         document.getElementById(`uploadForm-${uniqueUploadId}`).addEventListener('submit', function (event) {
-            event.preventDefault(); // 폼 제출 기본 동작 방지
-    
+            event.preventDefault();
+
             const periodStart = document.getElementById(`periodStart-${uniqueUploadId}`).value;
             const periodEnd = document.getElementById(`periodEnd-${uniqueUploadId}`).value;
             const role = document.getElementById(`role-${uniqueUploadId}`).value;
             const company = document.getElementById(`company-${uniqueUploadId}`).value;
             const description = document.getElementById(`description-${uniqueUploadId}`).value;
-    
-            // YYYY-MM 형식으로 입력된 날짜를 년-월 형식으로 변환
+
             const formatPeriod = (start, end) => {
                 const [startYear, startMonth] = start.split("-");
                 const [endYear, endMonth] = end.split("-");
                 return `${startYear}년 ${startMonth}월 ~ ${endYear}년 ${endMonth}월`;
             };
-    
+
             const periodFormatted = formatPeriod(periodStart, periodEnd);
-    
+
             fetch('/uploadresume', {
                 method: 'POST',
                 headers: {
@@ -132,39 +128,27 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(result => {
                 alert('Experience added successfully!');
-                document.getElementById(uniqueUploadId).remove(); // 새 카드 제거
+                document.getElementById(uniqueUploadId).remove();
             })
             .catch(error => console.error('Error:', error));
         });
-    
+
         document.getElementById(`cancelUpload-${uniqueUploadId}`).addEventListener('click', function () {
-            document.getElementById(uniqueUploadId).remove(); // 새 카드 제거
+            document.getElementById(uniqueUploadId).remove();
         });
     });
 
-    // 수정 버튼 클릭 시 카드 내에 수정 폼 추가하기
     experienceContainer.addEventListener('click', function (event) {
         if (event.target.closest('.edit-button')) {
             const card = event.target.closest('.card');
             const cardId = card.getAttribute('data-id');
-    
-            // 카드에서 기존 기간 데이터를 추출 (예: "2024년 12월 ~ 2025년 1월")
+
             const periodText = card.querySelector('.text-primary').textContent;
             const [startPeriod, endPeriod] = periodText.split(' ~ ').map(period => {
-                const yearMonth = period.trim().split('년 ');
-                
-                if (yearMonth.length < 2) {
-                    console.error('Invalid period format:', period);
-                    return '';  // 잘못된 형식이면 빈 문자열 반환
-                }
-                
-                const year = yearMonth[0].trim();
-                const month = yearMonth[1].replace('월', '').trim().padStart(2, '0'); // '월'을 제거하고 앞에 0을 채움
-                
-                return `${year}-${month}`;
+                const [year, month] = period.trim().split('년 ');
+                return `${year.trim()}-${month.replace('월', '').trim().padStart(2, '0')}`;
             });
-    
-            // 수정 카드 추가
+
             const editCardId = `editCard-${Date.now()}`;
             const editCard = `
                 <div class="card shadow border-0 rounded-4 mb-5" id="${editCardId}">
@@ -198,26 +182,26 @@ document.addEventListener("DOMContentLoaded", function () {
                     </div>
                 </div>
             `;
-    
+
             card.insertAdjacentHTML('beforeend', editCard);
-    
+
             document.getElementById(`editForm-${editCardId}`).addEventListener('submit', function (event) {
-                event.preventDefault(); // 폼 제출 기본 동작 방지
-    
+                event.preventDefault();
+
                 const periodStart = document.getElementById(`editPeriodStart-${editCardId}`).value;
                 const periodEnd = document.getElementById(`editPeriodEnd-${editCardId}`).value;
                 const role = document.getElementById(`editRole-${editCardId}`).value;
                 const company = document.getElementById(`editCompany-${editCardId}`).value;
                 const description = document.getElementById(`editDescription-${editCardId}`).value;
-    
+
                 const formatPeriod = (start, end) => {
                     const [startYear, startMonth] = start.split("-");
                     const [endYear, endMonth] = end.split("-");
                     return `${startYear}년 ${startMonth}월 ~ ${endYear}년 ${endMonth}월`;
                 };
-    
+
                 const periodFormatted = formatPeriod(periodStart, periodEnd);
-    
+
                 fetch('/editresume', {
                     method: 'POST',
                     headers: {
@@ -234,8 +218,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 .then(response => response.json())
                 .then(result => {
                     alert('Experience updated successfully!');
-                    document.getElementById(editCardId).remove(); // 수정 카드 제거
-                    // 여기에서 기존 카드 내용 업데이트 필요 (예: 카드의 기간, 역할, 회사 정보)
+                    document.getElementById(editCardId).remove();
                     card.querySelector('.text-primary').textContent = periodFormatted;
                     card.querySelector('.small.fw-bolder').textContent = role;
                     card.querySelector('.small.text-muted').textContent = company;
@@ -243,9 +226,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 })
                 .catch(error => console.error('Error:', error));
             });
-    
+
             document.getElementById(`cancelEdit-${editCardId}`).addEventListener('click', function () {
-                document.getElementById(editCardId).remove(); // 수정 카드 제거
+                document.getElementById(editCardId).remove();
             });
         }
     });
