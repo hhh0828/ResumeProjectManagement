c53387f44b2ec3ea23d7628ec7bbc463a578e5ea
hyunho.hong <hhhcjswo@naver.com>
2024-09-08 21:26:55 +0900

    t

--- home/sklang/sklangscripts.js
+++ home/sklang/sklangscripts.js
@@ -24,9 +24,13 @@ document.addEventListener("DOMContentLoaded", function () {
                                 <div class="row align-items-center gx-5">
                                     <div class="col text-center text-lg-start mb-4 mb-lg-0">
                                         <div class="bg-light p-4 rounded-4">
-                                            <div class="text-primary fw-bolder mb-2 date-period">${experience.period}</div>
+                                            <div class="date-container">
+                                            <div class="text-primary fw-bolder">${experience.period}</div>
+                                            </div>
+                                            <div class="text-container">
                                             <div class="small fw-bolder">${experience.role}</div>
                                             <div class="small text-muted">${experience.company}</div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-lg-8">
