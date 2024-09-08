7330f7c2072adf4ef88e6b006a8bcaf04908df9f
hyunho.hong <hhhcjswo@naver.com>
2024-09-08 17:05:36 +0900

    edit image scale and text temple in projects page

--- home/pj/pj.js
+++ home/pj/pj.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
                         
                     <div class="card-body p-0">
-                        <div class="d-flex align-items-center">
+                        <div class="d-flex align-items-center project-card">
                             <div class="p-5">
                                 <input type="hidden" name="post_id" value="${project.ID}}" data-id=${project.ID}}>
                                 <h2 class="fw-bolder">${project.Name}</h2>
