4f1c0374ce037eb01b880e5213abcf1aa2171771
hyunho.hong <hhhcjswo@naver.com>
2024-11-06 22:13:01 +0900

    js code edited

--- home/sklang/sklangscripts.js
+++ home/sklang/sklangscripts.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const description = convertLineBreaks(experience.description);
 
             const experienceCard = ` 
-                <div class="card shadow border-0 rounded-4 mb-5" data-id="=${experience.ID}">
+                <div class="card shadow border-0 rounded-4 mb-5" data-id=${experience.ID}>
                     <div class="card-body p-5">
                         <div class="row align-items-center gx-5">
                             <div class="col text-center text-lg-start mb-4 mb-lg-0">
@@ -254,7 +254,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        ID: parseInt(cardId, 10),
+                        ID: cardId,
                         period: periodFormatted,
                         role,
                         company,
